07d93b4ddcc6ac5e9bb0e47458133c6000ad107f
David Boyer <mangr3n@gmail.com>
2021-03-23 15:04:43 -0400

    Added States, Render Geography

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import App from './App.svelte';
 
 const app = new App({target:document.body});
 
-if (
-  import.meta.hot) {
+if (import.meta.hot) {
   import.meta.hot.accept();
   import.meta.hot.dispose(() => {
     app.$destroy();
